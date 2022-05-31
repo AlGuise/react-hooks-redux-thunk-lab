b91c096d2813e9654f91957d2a4f0f523b5a7a46
Alain Giessel <alain.giessel@gmail.com>
2022-05-31 12:52:46 -0600

    commit tree 7aa9881da845cbb2d74942b748f8f0f745eaf410

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,11 @@
-// write your CatList component here
+import React from "react";
+
+export default function CatList({ catPics = []}) {
+    return (
+        <div>
+            {catPics.map((pic)=> (
+                <img key={pic.id} src={pic.url} alt="cat" />
+            ))}
+        </div>
+    );
+}
